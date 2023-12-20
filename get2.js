650ee108a888b6a21385358a0fd197287494faad
IceScout26 <dhimasscout@gmail.com>
2023-12-20 21:37:22 +0700

    fix : space

--- get2.js
+++ get2.js
@@ -1,5 +1,6 @@
 import http from "k6/http";
 import { check } from "k6";
+
 export default function () {
   const res = http.get("https://jsonplaceholder.typicode.com/todos");
   const checkOutput = check(res, {
